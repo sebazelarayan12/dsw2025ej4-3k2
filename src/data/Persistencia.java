8b7287a0f0352cafe08f2243a3c83c9cc2c1066c
Zelarayan Sebastian <sebazelarayan12@gmail.com>
2025-04-13 23:03:47 -0300

    Agregado el menu

--- src/data/Persistencia.java
+++ src/data/Persistencia.java
@@ -9,6 +9,7 @@ public class Persistencia {
     private static ArrayList<Mamifero> animales = new ArrayList<>();
     private static ArrayList<Sector> sectores = new ArrayList<>();
     private static ArrayList<Especie> especies = new ArrayList<>();
+    private static ArrayList<Pais> paises = new ArrayList<>();
 
     private static void inicializarEspecies() {
         especies.add(new Especie("León", TipoAlimentacion.CARNIVORO, 0.2));
@@ -27,15 +28,24 @@ public class Persistencia {
     }
     
     private static void inicializarAnimales() throws InvalidPropertiesFormatException {
-        animales.add(new Carnivoro(5,250,especies.get(0), sectores.get(1)));
-        animales.add(new Carnivoro(2,180,especies.get(2), sectores.get(3)));
-        animales.add(new Herbivoro(3, 1020,especies.get(1), sectores.get(0), 170));
-        animales.add(new Herbivoro(8, 3800,especies.get(3), sectores.get(2), 320));
+        animales.add(new Carnivoro(5,250,especies.get(0), sectores.get(1), paises.get(0)));
+        animales.add(new Carnivoro(2,180,especies.get(2), sectores.get(3), paises.get(2)));
+        animales.add(new Herbivoro(3, 1020,especies.get(1), sectores.get(0), 170, paises.get(4)));
+        animales.add(new Herbivoro(8, 3800,especies.get(3), sectores.get(2), 320, paises.get(1)));
+    }
+    
+    private static void inicializarPaises(){
+        paises.add(new Pais("Madagascar","450"));
+        paises.add(new Pais("Rusia","643"));
+        paises.add(new Pais("India","356"));
+        paises.add(new Pais("Brasil","076"));
+        paises.add(new Pais("Australia","036"));
     }
 
     public static void inicializar() throws InvalidPropertiesFormatException{
         inicializarEspecies();
         inicializarSectores();
+        inicializarPaises();
         inicializarAnimales();
     }
 
@@ -51,11 +61,22 @@ public class Persistencia {
         return especies;
     }
 
+    
     public static double getTotalComida(TipoAlimentacion tipoAlimentacion) {
         double total = 0;
         for(Mamifero animal : animales){
             total += animal.TieneAlimentacion(tipoAlimentacion) ? animal.calcularCantidadDeComida() : 0;
         }
         return total;
+    }
+
+    public static void agregarAnimal(Mamifero mamifero){
+        
+        animales.add(mamifero);
+        
+    }
+    
+    public static ArrayList<Pais> getPaises() {
+        return paises;
     }
 }
--- src/views/Controlador.java
+++ src/views/Controlador.java
@@ -6,8 +6,46 @@ import domain.*;
 import java.util.ArrayList;
 import java.util.InvalidPropertiesFormatException;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class Controlador {
+    public static Agregaranimales agregaranimales;
+    public static Menuanimales menuanimales;
+    
+    public static void guardarAnimal(Mamifero mamifero){
+        Persistencia.agregarAnimal(mamifero);
+    }
+    
+     public static void salir(Menuanimales vista){
+        vista.dispose();
+    }
+    
+    public static void volver (Agregaranimales vista){
+        vista.dispose();
+        new Menuanimales().setVisible(true);
+        
+    }
+    
+    public static void volveer (ListarAnimalesView vista){
+        vista.dispose();
+        new Menuanimales().setVisible(true);
+        
+    }
+    
+    public static void vistaagregar (Menuanimales vista){
+        vista.dispose();
+        new Agregaranimales().setVisible(true);
+    }
+    
+    public static void vistalista (Menuanimales vista){
+        vista.dispose();
+        new ListarAnimalesView().setVisible(true);
+    }
+    
+    public static ArrayList<Pais> getPaises(){
+        return Persistencia.getPaises();
+    }
+  
     public static TipoAlimentacion[] getTiposAlimentacion(){
         return  TipoAlimentacion.values();
     }
@@ -26,9 +64,48 @@ public class Controlador {
         return animales;
     }
     
+    
+   public static void agregarAnimal (Mamifero mamifero){
+       Persistencia.agregarAnimal(mamifero);
+   }
+    
+  /*  public static void cargarTabla(ListarAnimalesView vista) {
+        //reconfigura el modelo que se aplica en la JTabla
+        vista.getModelo().setColumnCount(0);
+        vista.getModelo().setNumRows(0);
+        vista.getModelo().addColumn("Fabricante");
+        vista.getModelo().addColumn("Codigo");
+        vista.getModelo().addColumn("Descripcion");
+        vista.getModelo().addColumn("Cantidad");
+        vista.getModelo().addColumn("Precio");
+        // alimenta al modelo filas con los datos de las personas
+        for (Producto producto : lista.getLista()) {
+            Object[] fila = new Object[5];
+            fila[0] = producto.getTipo();
+            fila[1] = producto.getCodigo();
+            fila[2] = producto.getDescripcion();
+            fila[3] = producto.getCantidad();
+            fila[4] = producto.getPrecio();
+            vista.getModelo().addRow(fila);
+        }
+        vista.getTabla().setModel(vista.getModelo());
+        // 40 pixel de ancho a la columna 0
+        //vista.getTabla().getColumnModel().getColumn(0).setPreferredWidth(40);
+        //vista.getTabla().getColumnModel().getColumn(1).setPreferredWidth(10);
+    }*/
+  
+    
     public static ComidaViewModel  calcularComida(){
         double totalCarnivoros = Persistencia.getTotalComida(TipoAlimentacion.CARNIVORO);
         double totalHerbivoros = Persistencia.getTotalComida(TipoAlimentacion.HERBIVORO);
         return new ComidaViewModel(totalCarnivoros, totalHerbivoros);
     }
+    
+    
+   /*public void actualizar(ListarAnimalesView vista) {
+        listarAnimales(vista);
+      
+    }*/
+    
+    
 }
